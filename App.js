991f7d61a69fd327b5eeab30dcce4cb6b2bba1c6
Giovanny Gabriel <giovanny.gabrieldsn@gmail.com>
2023-01-23 14:49:33 -0300

    aplicando a interface do cronometro

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{} from 'react';
+import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <Image
+  source={require("./src/assets/crono.png")}
+  style={styles.img}/>
+ 
+  <Text style={styles.timer}>00;00;00</Text>
+  
+  <View style={styles.btnArea}>
+   <TouchableOpacity style={styles.btn}>
+   <Text style={styles.btnText}> Iniciar
+   </Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity style={styles.btn}>
+  <Text style={styles.btnText}> Parar
+  </Text>
+ </TouchableOpacity>
+  </View>
+
+
     </View>
   );
 }
@@ -13,8 +30,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#808080',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img:{
+  marginTop:130
+  },
+  timer:{
+    fontSize:40,
+    marginTop:-160,
+    fontWeight:'bold',
+    color: "#FFF"
+  },
+  btnArea:{
+    flex:1,
+    marginTop:130,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
